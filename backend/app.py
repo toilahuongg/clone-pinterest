5056ca415cd53fe6329fbea2c078db8540d9f758
toilahuong <vbh30062001@gmail.com>
2021-09-25 21:26:58 +0700

    init

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask import render_template
 import dotenv
 import os
-print(os.getenv('VERSION'))
 app = Flask(__name__)
 
 
+print(f"VERSION {os.getenv('VERSION')}")
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+if __name__ == '__main__':
+   app.run()
